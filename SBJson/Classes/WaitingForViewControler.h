35fc9df8b14f033ac755be857a1ee46391c9d7c1
Eagle Du <flyeagledu@163.com>
2012-02-20 19:50:18 +0800

    Json serializer

---
+++ SBJson/Classes/WaitingForViewControler.h
@@ -0,0 +1,13 @@
+//
+//  WaitingForViewControler.h
+//  SBJson
+//
+//  Created by EagleDu on 11/12/25.
+//  Copyright (c) 2011年 Stig Brautaset. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WaitingForViewControler : UIViewController
+
+@end
