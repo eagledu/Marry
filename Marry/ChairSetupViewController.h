d6eb19c628a9a9e9f54b27f7eba46635853cea18
Eagle Du <flyeagledu@163.com>
2012-02-26 20:09:23 +0800

    Setup View controller

---
+++ Marry/ChairSetupViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChairSetup.h
+//  Marry
+//
+//  Created by EagleDu on 12/2/26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChairSetup : UIViewController
+
+@end
